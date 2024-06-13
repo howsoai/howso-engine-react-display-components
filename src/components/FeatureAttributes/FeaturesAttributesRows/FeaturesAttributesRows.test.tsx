5633334772fd64ca4748b2340473b1e21f4c7afb
Lance Gliser <lance.gliser@howso.com>
2024-06-13 15:58:42 -0500

    Tests cleanup

--- src/components/FeatureAttributes/FeaturesAttributesRows/FeaturesAttributesRows.test.tsx
+++ src/components/FeatureAttributes/FeaturesAttributesRows/FeaturesAttributesRows.test.tsx
@@ -21,10 +21,6 @@ import {
   getFeatureAttributesActiveFeatureAtom,
   getFeatureAttributesOptionsAtom,
 } from "../hooks";
-import {
-  FeatureAttributeFormValues,
-  getFeatureAttributesFromFormData,
-} from "../utils";
 
 describe("FeaturesAttributesRows", () => {
   it("should open a configuration modal, save, and load the next", async () => {
@@ -139,60 +135,3 @@ const expectFeatureAttributesInDialog = async (
     ).toHaveTextContent(element);
   });
 };
-
-describe("getFeatureAttributesFromFormData", () => {
-  it("should remove empty values", () => {
-    const data: FeatureAttributeFormValues = {
-      type: "continuous",
-      data_type: "number",
-      is_datetime: false,
-      significant_digits: undefined,
-      decimal_places: undefined,
-      date_time_format: "",
-    };
-    const attributes = getFeatureAttributesFromFormData(data);
-    const toBeRemoved: (keyof FeatureAttributes)[] = [
-      "significant_digits",
-      "decimal_places",
-      "date_time_format",
-    ];
-    toBeRemoved.forEach((property) => {
-      expect(attributes).not.toHaveProperty(property);
-    });
-  });
-
-  it("should not remove 0's", () => {
-    const data: FeatureAttributeFormValues = {
-      type: "continuous",
-      data_type: "number",
-      is_datetime: false,
-      significant_digits: 0,
-      decimal_places: 0,
-    };
-    const attributes = getFeatureAttributesFromFormData(data);
-    const toBePreserve: (keyof FeatureAttributes)[] = [
-      "significant_digits",
-      "decimal_places",
-    ];
-    toBePreserve.forEach((property) => {
-      expect(attributes[property]).toBe(0);
-    });
-  });
-
-  it("should trim strings", () => {
-    const data: FeatureAttributeFormValues = {
-      type: "continuous",
-      data_type: "string",
-      is_datetime: true,
-      date_time_format: "YYYY-MM-DD ",
-      bounds: {
-        allowed: ["This little ", "piggy went to ", "market "],
-      },
-    };
-    const attributes = getFeatureAttributesFromFormData(data);
-    expect(attributes.date_time_format).toBe(data.date_time_format?.trim());
-    expect(attributes.bounds?.allowed?.join(" ")).toBe(
-      data.bounds?.allowed?.map((value) => value.trim())?.join(" "),
-    );
-  });
-});
---
+++ src/components/FeatureAttributes/utils/forms.test.ts
@@ -0,0 +1,62 @@
+import { FeatureAttributes } from "@howso/openapi-client";
+import {
+  FeatureAttributeFormValues,
+  getFeatureAttributesFromFormData,
+} from "./forms";
+
+describe("getFeatureAttributesFromFormData", () => {
+  it("should remove empty values", () => {
+    const data: FeatureAttributeFormValues = {
+      type: "continuous",
+      data_type: "number",
+      is_datetime: false,
+      significant_digits: undefined,
+      decimal_places: undefined,
+      date_time_format: "",
+    };
+    const attributes = getFeatureAttributesFromFormData(data);
+    const toBeRemoved: (keyof FeatureAttributes)[] = [
+      "significant_digits",
+      "decimal_places",
+      "date_time_format",
+    ];
+    toBeRemoved.forEach((property) => {
+      expect(attributes).not.toHaveProperty(property);
+    });
+  });
+
+  it("should not remove 0's", () => {
+    const data: FeatureAttributeFormValues = {
+      type: "continuous",
+      data_type: "number",
+      is_datetime: false,
+      significant_digits: 0,
+      decimal_places: 0,
+    };
+    const attributes = getFeatureAttributesFromFormData(data);
+    const toBePreserve: (keyof FeatureAttributes)[] = [
+      "significant_digits",
+      "decimal_places",
+    ];
+    toBePreserve.forEach((property) => {
+      expect(attributes[property]).toBe(0);
+    });
+  });
+
+  it("should trim strings", () => {
+    const data: FeatureAttributeFormValues = {
+      type: "continuous",
+      data_type: "string",
+      is_datetime: true,
+      date_time_format: "YYYY-MM-DD ",
+      bounds: {
+        allowed: ["This little ", "piggy went to ", "market "],
+      },
+    };
+    const attributes = getFeatureAttributesFromFormData(data);
+    expect(attributes.date_time_format).toBe(data.date_time_format?.trim());
+    expect(attributes.bounds?.allowed?.join(" ")).toBe(
+      data.bounds?.allowed?.map((value) => value.trim())?.join(" "),
+    );
+  });
+});
