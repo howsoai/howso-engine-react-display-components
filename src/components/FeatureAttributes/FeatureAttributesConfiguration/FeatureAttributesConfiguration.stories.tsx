d17876a561797eadf2da49a3deae3563086b6745
Lance Gliser <lance.gliser@howso.com>
2024-06-20 15:21:51 -0500

    Removed autodoc from FeatureAttributesConfiguration

--- src/components/FeatureAttributes/FeatureAttributesConfiguration/FeatureAttributesConfiguration.stories.tsx
+++ src/components/FeatureAttributes/FeatureAttributesConfiguration/FeatureAttributesConfiguration.stories.tsx
@@ -8,7 +8,7 @@ import { getFeaturesAttributesContextDecorator } from "../FeaturesAttributesCont
 const meta: Meta<typeof FeatureAttributesConfiguration> = {
   component: FeatureAttributesConfiguration,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
-  tags: ["autodocs"],
+  // tags: ["autodocs"],
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
