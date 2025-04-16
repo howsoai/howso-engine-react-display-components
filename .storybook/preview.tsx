82611e1a7f5bb13a85e787e494b8449a09549587
Lance Gliser <lance.gliser@howso.com>
2025-04-16 14:35:15 -0500

    0000: Updating Vite dependencies and Inter font (#36)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,33 +4,28 @@ import { Flowbite, ThemeMode } from "flowbite-react";
 import React, { Suspense } from "react";
 import { useTranslation } from "react-i18next";
 import { Await, RouterProvider, createMemoryRouter } from "react-router-dom";
+import { darkBackground, lightBackground } from "./constants";
 import i18n from "./i18next";
+import { isDarkBackground } from "./utils";
 // CSS
-// import "@fontsource/inter";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/700.css";
+import "@fontsource-variable/inter";
+import "@fontsource-variable/inter/wght-italic.css";
 import "@howso/react-tailwind-flowbite-components/lib/styles.css";
 import "./tailwind.css";
-
-const light = {
-  name: "Light",
-  value: "#fff",
-};
-
-const dark = {
-  name: "Dark",
-  value: "#000",
-};
 
 const preview: Preview = {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
-    // layout: "fullscreen", // TODO not having any effect..
     backgrounds: {
       // Load them by hand, or make a nice loop if you wish
-      values: [light, dark],
+      values: [lightBackground, darkBackground],
       // Ensure a default is set, so you avoid type errors reading from undefined!
-      default: light.name,
+      default: lightBackground.name,
+      // Supports globals usage
+      options: {
+        [lightBackground.name]: lightBackground,
+        [darkBackground.name]: darkBackground,
+      },
     },
     controls: {
       matchers: {
@@ -43,14 +38,12 @@ const preview: Preview = {
       viewport: { value: undefined },
     },
   },
-
   decorators: [
     (Story: StoryFn, context) => {
-      const mode: ThemeMode =
-        context.globals.backgrounds?.value === dark.value ? "dark" : "light";
-
       // Flowbite likes to store your preference in local storage, not what we're doing here.
       localStorage.removeItem("flowbite-theme-mode");
+
+      const mode: ThemeMode = isDarkBackground(context) ? "dark" : "light";
       if (mode === "dark") {
         document.documentElement.classList.add("dark");
       } else {
@@ -58,7 +51,7 @@ const preview: Preview = {
       }
 
       return (
-        <Flowbite theme={{ theme: standardFlowbiteTheme, mode: "light" }}>
+        <Flowbite theme={{ theme: standardFlowbiteTheme, mode }}>
           <Story />
         </Flowbite>
       );
@@ -82,7 +75,7 @@ const preview: Preview = {
     ),
   ],
 
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 };
 export default preview;
 ``;
